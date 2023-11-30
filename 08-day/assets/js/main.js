55569739293d66929dc67017dc0023f8411a2f93
Abdul Hamid <hamidkhattak469@gmail.com>
2023-11-30 17:46:49 +0500

    Work on Palindrome Checker and todo app

---
+++ 08-day/assets/js/main.js
@@ -0,0 +1,6 @@
+const palindromeChecker = () => {
+    let input_value = document.getElementById("palindrome-input").value;
+    let pi = document.getElementById("palindrome-info").innerHTML = input_value;
+    
+
+}
